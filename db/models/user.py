4b5e770dd69568246df463787bc3d0b25c5a9ceb
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-02-14 19:07:53 -0600

    Se cambio el tipo de dato del modelo User de str a EmailStr

--- db/models/user.py
+++ db/models/user.py
@@ -1,8 +1,8 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 # Modelo de usuario (estructura de datos esperada en la API)
 class User(BaseModel):
     id: Optional[str] = None
     username: str
-    email: str
+    email: EmailStr
