38cc55d0c3386abbcc127ac4f04b9ecd46b60dc8
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-02-14 18:29:22 -0600

    se agrego la configuración del puerto e ip de conexión en main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import uvicorn
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
@@ -10,11 +13,12 @@ app = FastAPI()
 app.include_router(users_db.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", tags=["root"])
-async def root():
-    return "Hola xd"
-
 
 @app.get("/url", tags=["root"])
 async def url():
-    return {"url": "https://api/v0"}
+    return {"message": "API is running on Railway!"}
+
+if __name__ == "__main__":
+    # Obtener el puerto asignado por Railway o usar 8000 como valor por defecto
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
